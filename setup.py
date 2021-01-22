9f5179a134c107cfc7cd79c44145987d6d4df355
Stijn <debackere@strw.leidenuniv.nl>
2021-01-22 19:32:35 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="gadget",
+    version="0.0.1",
+    author="Stijn Debackere",
+    author_email="debackere@strw.leidenuniv.nl",
+    description="A package to read in different gadget simulations.",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url="",
+    py_modules=['gadget'],
+    install_requires=[
+        "h5py",
+        "numpy",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
